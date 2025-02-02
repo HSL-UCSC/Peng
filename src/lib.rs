ec92de399eca6a43d4391663eeb7f9dd01417908
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2025-02-01 20:12:51 -0800

    relocate logging to logger

--- src/lib.rs
+++ src/lib.rs
@@ -95,6 +95,8 @@ pub enum SimulationError {
 /// let quadrotor = Quadrotor::new(time_step, config::SimulationConfig::default(), mass, gravity, drag_coefficient, inertia_matrix).unwrap();
 /// ```
 pub struct Quadrotor {
+    /// The name or ID of the quadrotor
+    pub name: String,
     /// Current position of the quadrotor in 3D space
     pub position: Vector3<f32>,
     /// Current velocity of the quadrotor
@@ -196,8 +198,9 @@ impl QuadrotorInterface for Quadrotor {
         Ok((self.acceleration, self.angular_velocity))
     }
 
-    fn vehicle_configuration(&self) -> config::QuadrotorConfig {
+    fn parameters(&self) -> config::QuadrotorConfig {
         config::QuadrotorConfig {
+            id: self.name.clone(),
             mass: self.mass,
             drag_coefficient: self.drag_coefficient,
             inertia_matrix: self.inertia_matrix.as_slice().try_into().unwrap(),
@@ -248,6 +251,7 @@ impl Quadrotor {
                     "Failed to invert inertia matrix".to_string(),
                 ))?;
         Ok(Self {
+            name: "PengQuad".to_string(),
             config,
             position: Vector3::zeros(),
             velocity: Vector3::zeros(),
@@ -632,8 +636,6 @@ impl PIDController {
     ) -> (f32, UnitQuaternion<f32>) {
         let error_position = desired_position - current_position;
         let error_velocity = desired_velocity - current_velocity;
-        dbg!(error_position);
-        dbg!(error_velocity);
         self.integral_pos_error += error_position * dt;
         self.integral_pos_error = self
             .integral_pos_error
@@ -2034,177 +2036,7 @@ pub fn ray_cast(
         Ok(f32::INFINITY)
     }
 }
-/// Logs simulation data to the rerun recording stream
-/// # Arguments
-/// * `rec` - The rerun::RecordingStream instance
-/// * `quad` - The Quadrotor instance
-/// * `desired_position` - The desired position vector
-/// * `measured_accel` - The measured acceleration vector
-/// * `measured_gyro` - The measured angular velocity vector
-/// # Errors
-/// * If the data cannot be logged to the recording stream
-/// # Example
-/// ```no_run
-/// use peng_quad::config;;
-/// use peng_quad::{Quadrotor, log_data, quadrotor::QuadrotorInterface};
-/// use nalgebra::Vector3;
-/// let rec = rerun::RecordingStreamBuilder::new("peng").connect().unwrap();
-/// let (time_step, mass, gravity, drag_coefficient) = (0.01, 1.3, 9.81, 0.01);
-/// let inertia_matrix = [0.0347563, 0.0, 0.0, 0.0, 0.0458929, 0.0, 0.0, 0.0, 0.0977];
-/// let mut quadrotor = Quadrotor::new(time_step, config::SimulationConfig::default(), mass, gravity, drag_coefficient, inertia_matrix).unwrap();
-/// let quad_state = quadrotor.observe(0).unwrap();
-/// let desired_position = Vector3::new(0.0, 0.0, 0.0);
-/// let desired_orientation = Vector3::new(0.0, 0.0, 0.0);
-/// let desired_velocity = Vector3::new(0.0, 0.0, 0.0);
-/// let measured_accel = Vector3::new(0.0, 0.0, 0.0);
-/// let measured_gyro = Vector3::new(0.0, 0.0, 0.0);
-/// let measured_torque = Vector3::new(0.0, 0.0, 0.0);
-/// log_data(&rec, &quad_state, &desired_position, &desired_orientation, &desired_velocity, &measured_accel, &measured_gyro, &measured_torque).unwrap();
-/// ```
-pub fn log_data(
-    rec: &rerun::RecordingStream,
-    quad_state: &QuadrotorState,
-    desired_position: &Vector3<f32>,
-    desired_orientation: &Vector3<f32>,
-    desired_velocity: &Vector3<f32>,
-    measured_accel: &Vector3<f32>,
-    measured_gyro: &Vector3<f32>,
-    torque: &Vector3<f32>,
-) -> Result<(), SimulationError> {
-    rec.log(
-        "world/quad/desired_position",
-        &rerun::Points3D::new([(desired_position.x, desired_position.y, desired_position.z)])
-            .with_radii([0.1])
-            .with_colors([rerun::Color::from_rgb(255, 255, 255)]),
-    )?;
-    rec.log(
-        "world/quad/base_link",
-        &rerun::Transform3D::from_translation_rotation(
-            rerun::Vec3D::new(
-                quad_state.position.x,
-                quad_state.position.y,
-                quad_state.position.z,
-            ),
-            rerun::Quaternion::from_xyzw([
-                quad_state.orientation.i,
-                quad_state.orientation.j,
-                quad_state.orientation.k,
-                quad_state.orientation.w,
-            ]),
-        )
-        .with_axis_length(0.7),
-    )?;
-    let (quad_roll, quad_pitch, quad_yaw) = quad_state.orientation.euler_angles();
-    let quad_euler_angles: Vector3<f32> = Vector3::new(quad_roll, quad_pitch, quad_yaw);
-    for (pre, vec) in [
-        ("position", &quad_state.position),
-        ("velocity", &quad_state.velocity),
-        ("accel", measured_accel),
-        ("orientation", &quad_euler_angles),
-        ("desired_orientation", &desired_orientation),
-        ("gyro", measured_gyro),
-        ("desired_position", desired_position),
-        ("desired_velocity", desired_velocity),
-        ("torque", torque),
-    ] {
-        for (i, a) in ["x", "y", "z"].iter().enumerate() {
-            rec.log(format!("{}/{}", pre, a), &rerun::Scalar::new(vec[i] as f64))?;
-        }
-    }
-    Ok(())
-}
 
-pub fn log_control(
-    rec: &rerun::RecordingStream,
-    thrust: f32,
-    torque: Vector3<f32>,
-    control_out: Option<(f32, Vector3<f32>)>,
-) -> Result<(), SimulationError> {
-    if let Some((thrust_ppm, torque_ppm)) = control_out {
-        rec.log("ppm/throttle", &rerun::Scalar::new(thrust_ppm as f64))?;
-        rec.log("control/thrust", &rerun::Scalar::new(thrust as f64))?;
-        for (i, a) in ["x", "y", "z"].iter().enumerate() {
-            rec.log(
-                format!("ppm/{}/{}", "torque", a),
-                &rerun::Scalar::new(torque_ppm[i] as f64),
-            )?;
-        }
-        for (i, a) in ["x", "y", "z"].iter().enumerate() {
-            rec.log(
-                format!("control/{}/{}", "torque", a),
-                &rerun::Scalar::new(torque[i] as f64),
-            )?;
-        }
-    };
-    Ok(())
-}
-
-/// Log the maze tube to the rerun recording stream
-/// # Arguments
-/// * `rec` - The rerun::RecordingStream instance
-/// * `maze` - The maze instance
-/// # Errors
-/// * If the data cannot be logged to the recording stream
-/// # Example
-/// ```no_run
-/// use peng_quad::{Maze, log_maze_tube};
-/// use rerun::RecordingStreamBuilder;
-/// let rec = rerun::RecordingStreamBuilder::new("log.rerun").connect().unwrap();
-/// let mut maze = Maze::new([-1.0, -1.0, -1.0], [1.0, 1.0, 1.0], 5, [0.1, 0.1, 0.1], [0.1, 0.5]);
-/// log_maze_tube(&rec, &maze).unwrap();
-/// ```
-pub fn log_maze_tube(rec: &rerun::RecordingStream, maze: &Maze) -> Result<(), SimulationError> {
-    let (lower_bounds, upper_bounds) = (maze.lower_bounds, maze.upper_bounds);
-    let center_position = rerun::external::glam::Vec3::new(
-        (lower_bounds[0] + upper_bounds[0]) / 2.0,
-        (lower_bounds[1] + upper_bounds[1]) / 2.0,
-        (lower_bounds[2] + upper_bounds[2]) / 2.0,
-    );
-    let half_sizes = rerun::external::glam::Vec3::new(
-        (upper_bounds[0] - lower_bounds[0]) / 2.0,
-        (upper_bounds[1] - lower_bounds[1]) / 2.0,
-        (upper_bounds[2] - lower_bounds[2]) / 2.0,
-    );
-    rec.log(
-        "world/maze/tube",
-        &rerun::Boxes3D::from_centers_and_half_sizes([center_position], [half_sizes])
-            .with_colors([rerun::Color::from_rgb(128, 128, 255)]),
-    )?;
-    Ok(())
-}
-/// Log the maze obstacles to the rerun recording stream
-/// # Arguments
-/// * `rec` - The rerun::RecordingStream instance
-/// * `maze` - The maze instance
-/// # Errors
-/// * If the data cannot be logged to the recording stream
-/// # Example
-/// ```no_run
-/// use peng_quad::{Maze, log_maze_obstacles};
-/// let rec = rerun::RecordingStreamBuilder::new("log.rerun").connect().unwrap();
-/// let mut maze = Maze::new([-1.0, -1.0, -1.0], [1.0, 1.0, 1.0], 5, [0.1, 0.1, 0.1], [0.1, 0.5]);
-/// log_maze_obstacles(&rec, &maze).unwrap();
-/// ```
-pub fn log_maze_obstacles(
-    rec: &rerun::RecordingStream,
-    maze: &Maze,
-) -> Result<(), SimulationError> {
-    let (positions, radii): (Vec<(f32, f32, f32)>, Vec<f32>) = maze
-        .obstacles
-        .iter()
-        .map(|obstacle| {
-            let pos = obstacle.position;
-            ((pos.x, pos.y, pos.z), obstacle.radius)
-        })
-        .unzip();
-    rec.log(
-        "world/maze/obstacles",
-        &rerun::Points3D::new(positions)
-            .with_radii(radii)
-            .with_colors([rerun::Color::from_rgb(255, 128, 128)]),
-    )?;
-    Ok(())
-}
 /// A struct to hold trajectory data
 /// # Example
 /// ```
@@ -2212,6 +2044,7 @@ pub fn log_maze_obstacles(
 /// let initial_point = nalgebra::Vector3::new(0.0, 0.0, 0.0);
 /// let mut trajectory = Trajectory::new(initial_point);
 /// ```
+#[derive(Clone, Debug)]
 pub struct Trajectory {
     /// A vector of 3D points
     pub points: Vec<Vector3<f32>>,
@@ -2262,199 +2095,6 @@ impl Trajectory {
             false
         }
     }
-}
-/// log trajectory data to the rerun recording stream
-/// # Arguments
-/// * `rec` - The rerun::RecordingStream instance
-/// * `trajectory` - The Trajectory instance
-/// # Errors
-/// * If the data cannot be logged to the recording stream
-/// # Example
-/// ```no_run
-/// use peng_quad::{Trajectory, log_trajectory};
-/// use nalgebra::Vector3;
-/// let rec = rerun::RecordingStreamBuilder::new("log.rerun").connect().unwrap();
-/// let mut trajectory = Trajectory::new(nalgebra::Vector3::new(0.0, 0.0, 0.0));
-/// trajectory.add_point(nalgebra::Vector3::new(1.0, 0.0, 0.0));
-/// log_trajectory(&rec, &trajectory).unwrap();
-/// ```
-pub fn log_trajectory(
-    rec: &rerun::RecordingStream,
-    trajectory: &Trajectory,
-) -> Result<(), SimulationError> {
-    let path = trajectory
-        .points
-        .iter()
-        .map(|p| (p.x, p.y, p.z))
-        .collect::<Vec<(f32, f32, f32)>>();
-    rec.log(
-        "world/quad/path",
-        &rerun::LineStrips3D::new([path]).with_colors([rerun::Color::from_rgb(0, 255, 255)]),
-    )?;
-    Ok(())
-}
-
-/// Log depth image data to the rerun recording stream
-///
-/// When the depth value is `f32::INFINITY`, the pixel is considered invalid and logged as black
-/// When the resolution is larger than 32x24, multi-threading can accelerate the rendering
-/// # Arguments
-/// * `rec` - The rerun::RecordingStream instance
-/// * `cam` - The Camera instance
-/// * `use_multi_threading` - Whether to use multithreading to log the depth image
-/// # Errors
-/// * If the data cannot be logged to the recording stream
-/// # Example
-/// ```no_run
-/// use peng_quad::{log_depth_image, Camera};
-/// let rec = rerun::RecordingStreamBuilder::new("log.rerun").connect().unwrap();
-/// let camera = Camera::new((640, 480), 0.1, 100.0, 60.0);
-/// let use_multi_threading = false;
-/// log_depth_image(&rec, &camera, use_multi_threading).unwrap();
-/// let use_multi_threading = true;
-/// log_depth_image(&rec, &camera, use_multi_threading).unwrap();
-/// ```
-pub fn log_depth_image(
-    rec: &rerun::RecordingStream,
-    cam: &Camera,
-    use_multi_threading: bool,
-) -> Result<(), SimulationError> {
-    let (width, height) = (cam.resolution.0, cam.resolution.1);
-    let (min_depth, max_depth) = (cam.near, cam.far);
-    let depth_image = &cam.depth_buffer;
-    let mut image: rerun::external::ndarray::Array<u8, _> =
-        rerun::external::ndarray::Array::zeros((height, width, 3));
-    let depth_range = max_depth - min_depth;
-    let scale_factor = 255.0 / depth_range;
-    if use_multi_threading {
-        const CHUNK_SIZE: usize = 32;
-        image
-            .as_slice_mut()
-            .expect("Failed to get mutable slice of image")
-            .par_chunks_exact_mut(CHUNK_SIZE * 3)
-            .enumerate()
-            .for_each(|(chunk_index, chunk)| {
-                let start_index = chunk_index * 32;
-                for (i, pixel) in chunk.chunks_exact_mut(3).enumerate() {
-                    let idx = start_index + i;
-                    let depth = depth_image[idx];
-                    let color = if depth.is_finite() {
-                        let normalized_depth =
-                            ((depth - min_depth) * scale_factor).clamp(0.0, 255.0);
-                        color_map_fn(normalized_depth)
-                    } else {
-                        (0, 0, 0)
-                    };
-                    (pixel[0], pixel[1], pixel[2]) = color;
-                }
-            });
-    } else {
-        for (index, pixel) in image
-            .as_slice_mut()
-            .expect("Failed to get mutable slice of image")
-            .chunks_exact_mut(3)
-            .enumerate()
-        {
-            let depth = depth_image[index];
-            let color = if depth.is_finite() {
-                let normalized_depth = ((depth - min_depth) * scale_factor).clamp(0.0, 255.0);
-                color_map_fn(normalized_depth)
-            } else {
-                (0, 0, 0)
-            };
-            (pixel[0], pixel[1], pixel[2]) = color;
-        }
-    }
-    let rerun_image = rerun::Image::from_color_model_and_tensor(rerun::ColorModel::RGB, image)
-        .map_err(|e| SimulationError::OtherError(format!("Failed to create rerun image: {}", e)))?;
-    rec.log("world/quad/cam/depth", &rerun_image)?;
-    Ok(())
-}
-/// creates pinhole camera
-/// # Arguments
-/// * `rec` - The rerun::RecordingStream instance
-/// * `cam` - The camera object
-/// * `cam_position` - The position vector of the camera (aligns with the quad)
-/// * `cam_orientation` - The orientation quaternion of quad
-/// * `cam_transform` - The transform matrix between quad and camera alignment
-/// # Errors
-/// * If the data cannot be logged to the recording stream
-/// # Example
-/// ```no_run
-/// use peng_quad::{log_pinhole_depth, Camera};
-/// use nalgebra::{Vector3, UnitQuaternion};
-/// let rec = rerun::RecordingStreamBuilder::new("log.rerun").connect().unwrap();
-/// let depth_image = vec![ 0.0f32 ; 640 * 480];
-/// let cam_position = Vector3::new(0.0,0.0,0.0);
-/// let cam_orientation = UnitQuaternion::identity();
-/// let cam_transform = [0.0, 0.0, 1.0, -1.0, 0.0, 0.0, 0.0, -1.0, 0.0];
-/// let camera = Camera::new((800, 600), 60.0, 0.1, 100.0);
-/// log_pinhole_depth(&rec, &camera, cam_position, cam_orientation, cam_transform).unwrap();
-/// ```
-pub fn log_pinhole_depth(
-    rec: &rerun::RecordingStream,
-    cam: &Camera,
-    cam_position: Vector3<f32>,
-    cam_orientation: UnitQuaternion<f32>,
-    cam_transform: [f32; 9],
-) -> Result<(), SimulationError> {
-    let depth_image = &cam.depth_buffer;
-    let (width, height) = cam.resolution;
-    let pinhole_camera = rerun::Pinhole::from_focal_length_and_resolution(
-        (cam.horizontal_focal_length, cam.vertical_focal_length),
-        (width as f32, height as f32),
-    )
-    .with_camera_xyz(rerun::components::ViewCoordinates::RDF)
-    .with_resolution((width as f32, height as f32))
-    .with_principal_point((width as f32 / 2.0, height as f32 / 2.0));
-    let rotated_camera_orientation = UnitQuaternion::from_rotation_matrix(
-        &(cam_orientation.to_rotation_matrix()
-            * Rotation3::from_matrix_unchecked(Matrix3::from_row_slice(&cam_transform))),
-    );
-    let cam_transform = rerun::Transform3D::from_translation_rotation(
-        rerun::Vec3D::new(cam_position.x, cam_position.y, cam_position.z),
-        rerun::Quaternion::from_xyzw([
-            rotated_camera_orientation.i,
-            rotated_camera_orientation.j,
-            rotated_camera_orientation.k,
-            rotated_camera_orientation.w,
-        ]),
-    );
-    rec.log("world/quad/cam", &cam_transform)?;
-    rec.log("world/quad/cam", &pinhole_camera)?;
-    let depth_image_rerun =
-        rerun::external::ndarray::Array::from_shape_vec((height, width), depth_image.to_vec())
-            .unwrap();
-    rec.log(
-        "world/quad/cam/rerun_depth",
-        &rerun::DepthImage::try_from(depth_image_rerun)
-            .unwrap()
-            .with_meter(1.0),
-    )?;
-
-    Ok(())
-}
-/// turbo color map function
-/// # Arguments
-/// * `gray` - The gray value in the range [0, 255]
-/// # Returns
-/// * The RGB color value in the range [0, 255]
-/// # Example
-/// ```
-/// use peng_quad::color_map_fn;
-/// let color = color_map_fn(128.0);
-/// ```
-#[inline]
-pub fn color_map_fn(gray: f32) -> (u8, u8, u8) {
-    let x = gray / 255.0;
-    let r = (34.61
-        + x * (1172.33 - x * (10793.56 - x * (33300.12 - x * (38394.49 - x * 14825.05)))))
-        .clamp(0.0, 255.0) as u8;
-    let g = (23.31 + x * (557.33 + x * (1225.33 - x * (3574.96 - x * (1073.77 + x * 707.56)))))
-        .clamp(0.0, 255.0) as u8;
-    let b = (27.2 + x * (3211.1 - x * (15327.97 - x * (27814.0 - x * (22569.18 - x * 6838.66)))))
-        .clamp(0.0, 255.0) as u8;
-    (r, g, b)
 }
 
 /// log joystick positions
