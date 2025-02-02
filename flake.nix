d66655a9d0e4cbf52523119f23cc0bd4da36fcdd
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2025-02-01 20:10:18 -0800

    flake

--- flake.nix
+++ flake.nix
@@ -1,51 +1,93 @@
 {
+
   description = "Rust development environment with Cargo dependencies";
 
-  # Specify the inputs, such as nixpkgs
   inputs = {
+    naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  # Define the outputs
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    # Define a devShell for development
-    devShell = pkgs.mkShell {
-      # Add Rust and Cargo to the environment
-      buildInputs = [
-        pkgs.rust-analyzer
-        pkgs.protobuf
-        pkgs.rustup
-        pkgs.zsh
-        pkgs.clang
-        pkgs.cmake
-        pkgs.cargo-binstall
-        pkgs.rerun
-      ];
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy rerun];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
 
-      # Optionally, set environment variables
-      CARGO_HOME = "./.cargo";
-      RUST_BACKTRACE = "1"; # Enable backtrace for debugging
+          shellHook = ''
+            export GIT_CONFIG=$PWD/.gitconfig
+            export CARGO_NET_GIT_FETCH_WITH_CLI=true
+            export LD_LIBRARY_PATH=/Users/m0/Developer/vicon-sys/vendor/libvicon/
+            export GIT_SSH_COMMAND="ssh -F ~/.ssh/config"  # Ensure it uses your SSH config
+            # Start Zsh if not already the active shell
+            echo "Entering Rust development environment..."
+            rustup install stable
+            cargo fetch # Pre-fetch dependencies defined in Cargo.toml
+            if [ "$SHELL" != "$(command -v zsh)" ]; then
+              export PATH="$HOME/.cargo/bin:$PATH"
+              export SHELL="$(command -v zsh)"
+              exec zsh
+            fi
+          '';
+        };
 
-      # Optional shellHook to fetch dependencies when entering the shell
-      shellHook = ''
-        export GIT_CONFIG=$PWD/.gitconfig
-        export CARGO_NET_GIT_FETCH_WITH_CLI=true
-        export LD_LIBRARY_PATH=/Users/m0/Developer/vicon-sys/vendor/libvicon/
-        export GIT_SSH_COMMAND="ssh -F ~/.ssh/config"  # Ensure it uses your SSH config
-        # Start Zsh if not already the active shell
-        echo "Entering Rust development environment..."
-        rustup install stable
-        cargo fetch # Pre-fetch dependencies defined in Cargo.toml
-        if [ "$SHELL" != "$(command -v zsh)" ]; then
-          export PATH="$HOME/.cargo/bin:$PATH"
-          export SHELL="$(command -v zsh)"
-          exec zsh
-        fi
-      '';
-    };
-  });
+      }
+    );
 }
-
+# {
+#   description = "Rust development environment with Cargo dependencies";
+#
+#   # Specify the inputs, such as nixpkgs
+#   inputs = {
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+#     flake-utils.url = "github:numtide/flake-utils";
+#   };
+#
+#   # Define the outputs
+#   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+#     pkgs = import nixpkgs { inherit system; };
+#   in {
+#     # Define a devShell for development
+#     devShell = pkgs.mkShell {
+#       # Add Rust and Cargo to the environment
+#       buildInputs = [
+#         pkgs.rust-analyzer
+#         pkgs.protobuf
+#         pkgs.rustup
+#         pkgs.zsh
+#         pkgs.clang
+#         pkgs.cmake
+#         pkgs.cargo-binstall
+#         pkgs.rerun
+#       ];
+#
+#       # Optionally, set environment variables
+#       CARGO_HOME = "./.cargo";
+#       RUST_BACKTRACE = "1"; # Enable backtrace for debugging
+#
+#       # Optional shellHook to fetch dependencies when entering the shell
+#       shellHook = ''
+#         export GIT_CONFIG=$PWD/.gitconfig
+#         export CARGO_NET_GIT_FETCH_WITH_CLI=true
+#         export LD_LIBRARY_PATH=/Users/m0/Developer/vicon-sys/vendor/libvicon/
+#         export GIT_SSH_COMMAND="ssh -F ~/.ssh/config"  # Ensure it uses your SSH config
+#         # Start Zsh if not already the active shell
+#         echo "Entering Rust development environment..."
+#         rustup install stable
+#         cargo fetch # Pre-fetch dependencies defined in Cargo.toml
+#         if [ "$SHELL" != "$(command -v zsh)" ]; then
+#           export PATH="$HOME/.cargo/bin:$PATH"
+#           export SHELL="$(command -v zsh)"
+#           exec zsh
+#         fi
+#       '';
+#     };
+#   });
+# }
+#
