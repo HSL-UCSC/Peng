993cb8ed24c0f1f21d972bd0d02073306e34bc11
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2025-02-17 15:16:57 -0800

    remove unused notify on worker sync

--- src/sync.rs
+++ src/sync.rs
@@ -5,7 +5,6 @@ use std::sync::{
 use tokio::sync::{Barrier, Mutex, Notify};
 
 pub struct WorkerSync {
-    pub notify: Arc<Notify>,
     pub clock: Arc<AtomicU64>,
     pub start_barrier: Arc<Barrier>,
     pub end_barrier: Arc<Barrier>,
