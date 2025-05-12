a6fc5fbe9e2a59186ad409545ad6bc59aeb2e6f3
Ryan A Rodriguez <ryan.and.rodriguez@gmail.com>
2025-05-12 00:21:02 -0700

    point flake and build script to the hsl oa apis

--- build.rs
+++ build.rs
@@ -9,14 +9,15 @@ fn run_tonic() {
     let proto_path = env::var("OBSTACLE_AVOIDANCE_APIS")
         .expect("OBSTACLE_AVOIDANCE_APIS not set");
 
+    println!("{}", proto_path);
     let proto_files = vec![
-        format!("{}/hyrl/v1/hyrl.proto", proto_path),
+        format!("{}/protos/hyrl_api/obstacle_avoidance.proto", proto_path),
     ];
 
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
-        .compile(&proto_files, &[proto_path])
+        .compile_protos(&proto_files, &[proto_path])
         .expect("Failed to compile proto files");
 
     for proto in &proto_files {
--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     oaapis = {
-      url = "github:friend0/ObstacleAvoidanceAPIs";
+      url   = "github:HSL-UCSC/Quad-RL/rar/server_init";
       flake = false;
     };
   };
